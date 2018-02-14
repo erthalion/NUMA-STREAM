f45423cb3b6a076d78e791976e79e05587b5e50d
erthalion <9erthalion6@gmail.com>
2018-02-14 20:40:25 +0100

    Update libraries and headers

--- run-stream.sh
+++ run-stream.sh
@@ -2,30 +2,30 @@
 
 for i in 1 2 4 8 12 24 36 48
 do
-  gcc -O2 -std=c99 -lpthread -lnuma -L ../numactl-2.0.7-rc2 -DNUM_THREADS=$i -DMEM_OFF=0 stream-pthreads.c
+  gcc -O2 -std=c99 -DNUM_THREADS=$i -DMEM_OFF=0 -DNON_NUMA stream-pthreads.c -pthread -lpthread -lnuma -o test_no_off
   echo "No off $i"
-  ./a.out > NO_OFF_$i
+  ./test_no_off > NO_OFF_$i
 done
 
 
 for i in 1 2 4 8 12 24 36 48
 do
-  gcc -O2 -std=c99 -lpthread -lnuma -L ../numactl-2.0.7-rc2 -DNUM_THREADS=$i -DMEM_OFF=1 stream-pthreads.c
+  gcc -O2 -std=c99 -DNUM_THREADS=$i -DMEM_OFF=1 -DNON_NUMA stream-pthreads.c -pthread -lpthread -lnuma  -o test_off
   echo "Off $i"
-  ./a.out > OFF_$i
+  ./test_off > OFF_$i
 done
 
 for i in 1 2 4 8 12 24 36 48
 do
-  gcc -O2 -std=c99 -lpthread -lnuma -L ../numactl-2.0.7-rc2 -DNUM_THREADS=$i -DMEM_OFF=0 -DSTRIDE=1 stream-pthreads.c
+  gcc -O2 -std=c99 -DNUM_THREADS=$i -DMEM_OFF=0 -DSTRIDE=1 -DNON_NUMA stream-pthreads.c -pthread -lpthread -lnuma  -o test_no_off_no_stride
   echo "No off $i"
-  ./a.out > NO_OFF_NO_STRIDE_$i
+  ./test_no_off_no_stride > NO_OFF_NO_STRIDE_$i
 done
 
 
 for i in 1 2 4 8 12 24 36 48
 do
-  gcc -O2 -std=c99 -lpthread -lnuma -L ../numactl-2.0.7-rc2 -DNUM_THREADS=$i -DMEM_OFF=1 -DSTRIDE=1 stream-pthreads.c
+  gcc -O2 -std=c99 -DNUM_THREADS=$i -DMEM_OFF=1 -DSTRIDE=1 -DNON_NUMA stream-pthreads.c -pthread -lpthread -lnuma  -o test_off_no_stride
   echo "Off $i"
-  ./a.out > OFF_NO_STRIDE_$i
+  ./test_off_no_stride > OFF_NO_STRIDE_$i
 done
